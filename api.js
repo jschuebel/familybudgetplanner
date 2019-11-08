5c8a9026d21582beea5938313af742bc0530960d
Jim Schuebel <schuebelsoft@yahoo.com>
2019-11-08 07:34:45 -0600

    fix load; add file loads

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 //const db = require('./db1');
 
 
@@ -13,13 +14,52 @@ let response = {
   
 
 router.get('/products', (req, res) => {
-	response.data = [{ ProductID: 1, Title: "Water", Count: 24, Cost: 1.99},{ ProductID: 2, Title: "Coffee Water", Count: 6, Cost: 2.99}];
-	res.json(response);
+
+	fs.readFile('Data/Products.json', (err, data) => {
+		if (err) throw err;
+		response.data = JSON.parse(data);
+		res.json(response);
+		/*    console.log('**after date cvt loop dump');
+		students.forEach((element, index, array) => {
+			console.log(index, element);
+			element.PurchaseDate=new Date(element.PurchaseDate);
+		});
+	*/	
+	});
+//	response.data = [{ ProductID: 1, Title: "Water", Count: 24, Cost: 1.99},{ ProductID: 2, Title: "Coffee Water", Count: 6, Cost: 2.99}];
 });
 
 router.get('/purchases', (req, res) => {
-	response.data = [{ ProductID: 1, Count: 5},{ ProductID: 2, Count: 2}];
+		fs.readFile('Data/Purchases.json', (err, data) => {
+		if (err) throw err;
+		response.data = JSON.parse(data);
+		res.json(response);
+	});
+
+});
+router.get('/categories', (req, res) => {
+	let rawdata = fs.readFileSync('Data/Categories.json');
+	response.data = JSON.parse(data);
 	res.json(response);
+	/*
+	fs.readFile('Data/Categories.json', (err, data) => {
+		if (err) throw err;
+		response.data = JSON.parse(data);
+		res.json(response);
+	});
+*/
+});
+router.get('/categoryxref', (req, res) => {
+	let rawdata = fs.readFileSync('Data/CategoryXref.json');
+	response.data = JSON.parse(data);
+	res.json(response);
+	/*
+	fs.readFile('Data/CategoryXref.json', (err, data) => {
+		if (err) throw err;
+		response.data = JSON.parse(data);
+		res.json(response);
+	});
+*/	
 });
 
 
